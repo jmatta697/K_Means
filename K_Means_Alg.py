df1c6d9bf1ab3872597d308d84c86d2e348e7969
Joe Matta <jmatta1980@hotmail.com>
2019-12-08 20:06:08 -0500

    Complete Project (Rough Draft)

--- K_Means_Alg.py
+++ K_Means_Alg.py
@@ -3,21 +3,29 @@ import numpy as np
 from random import uniform
 from math import sqrt
 from sys import maxsize
+from sys import exit
 import matplotlib.pyplot as plt
+from time import sleep
 
 
 class KMeansAlg:
-    def __init__(self, x_data, y_data, k_centroids, iterations):
+    def __init__(self, x_data, y_data, k_centroids, iterations, x_axis, y_axis):
         self.x = x_data
         self.y = y_data
         self.k = k_centroids
         self.iter = iterations
+        self.current_iter = 0
         self.x_min = min(x_data)
         self.x_max = max(x_data)
         self.y_min = min(y_data)
         self.y_max = max(y_data)
         self.centroid_point_list = []
+        self.cluster_objects = []
+        # point group dictionary
         self.centroid_point_groups = {}
+        # graph labels
+        self.x_axis_graph_label = x_axis
+        self.y_axis_graph_label = y_axis
         # get random centroid start points
         for i in range(k_centroids):
             new_cent = [0, 0]
@@ -30,23 +38,61 @@ class KMeansAlg:
         # initialize point group dictionary with initial centroids
         # construct point pairs
         self.data_points = np.array(list(zip(x_data, y_data)))
+        self.colors_assigned = False
+
+    # cluster objects to associate color and SSE with cluster data
+    class ClusterObject:
+        def __init__(self, color_string, cluster_centroid, point_lst):
+            self.sum_squared_error = 0
+            self.centroid_point = cluster_centroid
+            self.point_list = point_lst
+            # interpret color string and associate color string
+            # colors = 5 * ["g.", "r.", "c.", "b.", "y.", "m.", "k."]
+            color_association = {
+                'g': "green",
+                'r': "red",
+                'c': "cyan",
+                'b': "blue",
+                'y': "yellow",
+                'm': "magenta",
+                'k': "black"
+            }
+            self.color = color_association[color_string[0]]
+            # calculate SSE...
+
+        # calculated sum squared error for one cluster - takes centroid point and list of cluster points
+        def calculate_sum_squared_error(self):
+            for point in self.point_list:
+                # calculate euclidean distance
+                dist = two_dimensional_euclidean_distance(self.centroid_point, point)
+                # add to error total
+                self.sum_squared_error = self.sum_squared_error + (dist ** 2)
 
     def run_algorithm(self):
-        self._construct_points_groups()
-        # DEBUG
-        print(self.centroid_point_groups)
-        # for cent in self.centroid_point_list:
-        #     print(self.centroid_point_groups[tuple(cent)])
+        for i in range(self.iter):
+            self.current_iter = i
+            self._construct_points_groups()
+            # DEBUG
+            # print(self.centroid_point_groups)
+            self._construct_plot()
+            # print cluster objects...
+            # reassign attributes of cluster objects
+            print()
+            cluster_obj_index = 0
+            for group in self.centroid_point_groups:
+                self.cluster_objects[cluster_obj_index].centroid_point = group
+                self.cluster_objects[cluster_obj_index].point_list = self.centroid_point_groups.get(group)
+                print(f'color: {self.cluster_objects[cluster_obj_index].color} | '
+                      f'cent: {self.cluster_objects[cluster_obj_index].centroid_point} | '
+                      f'point list: {self.cluster_objects[cluster_obj_index].point_list}')
+                cluster_obj_index = cluster_obj_index + 1
+            sleep(1)
+            self._recalculate_centroid_positions()
+        # calculate SSE and add it to cluster object
         print()
-        self._construct_plot()
-        # -----------------------------------------------------
-        self._recalculate_centroid_positions()
-        # for cent in self.centroid_point_list:
-        #     print(self.centroid_point_groups[tuple(cent)])
-        self._construct_points_groups()
-        print(self.centroid_point_groups)
-        print()
-        self._construct_plot()
+        for obj in self.cluster_objects:
+            obj.calculate_sum_squared_error()
+            print(f'SSE for {obj.color}: {obj.sum_squared_error}')
 
     def _construct_points_groups(self):
         for pt in self.data_points:
@@ -76,8 +122,12 @@ class KMeansAlg:
                 x_total = x_total + point[0]
                 y_total = y_total + point[1]
             # calculate x coordinate average
-            x_average = x_total / num_points
-            y_average = y_total / num_points
+            try:
+                x_average = x_total / num_points
+                y_average = y_total / num_points
+            except ZeroDivisionError:
+                print("\n*** There are clusters with zero points. Restart algorithm. *** EXITING...")
+                exit()
             temp_centroid_point_list.append([x_average, y_average])
             temp_point_group_dict[tuple([x_average, y_average])] = []
         self.centroid_point_list = temp_centroid_point_list
@@ -91,17 +141,29 @@ class KMeansAlg:
         # i is used to index colors - now set to a max of 15 groups
         i = 0
         for centroid_key in self.centroid_point_groups:
-            plt.plot(centroid_key[0], centroid_key[1], 'k+', markersize=14)
             # set color
             point_color = colors[i]
+            # assign colors to cluster objects here - only do this once
+            # ----
+            if not self.colors_assigned:
+                # construct cluster object and add it to the cluster object list
+                # single_dict_item = {self.centroid_point_groups[centroid_key]}
+                cluster_obj = self.ClusterObject(point_color, centroid_key, self.centroid_point_groups[centroid_key])
+                self.cluster_objects.append(cluster_obj)
+            # ----
             for point in self.centroid_point_groups[centroid_key]:
                 plt.plot(point[0], point[1], colors[i], markersize=6, label='_nolegend_')
             i = i + 1
+            plt.plot(centroid_key[0], centroid_key[1], 'k+', markersize=14)
+
+        self.colors_assigned = True
+        plt.xlabel(self.x_axis_graph_label)
+        plt.ylabel(self.y_axis_graph_label)
+        plt.title(f'Image Segmentation ({self.x_axis_graph_label} vs. {self.y_axis_graph_label}) [K={self.k}] '
+                  f'\n\nITERATION = {self.current_iter}', fontsize=10)
         plt.show()
 
 
 def two_dimensional_euclidean_distance(p1, p2):
     dist = sqrt(((p2[0]-p1[0])**2) + ((p2[1]-p1[1])**2))
     return dist
-
-
--- Main.py
+++ Main.py
@@ -1,11 +1,6 @@
 
 # K-Means algorithm
-
 import pandas as pd
-import matplotlib.pyplot as plt
-# scikit-learn.org/stable/index.html
-from sklearn.cluster import KMeans
-import numpy as np
 from K_Means_Alg import KMeansAlg
 
 
@@ -21,50 +16,12 @@ def main():
     y_data = data[y]
     # print(x_data)
 
-    # make (x, y) pairs..
-    # point_pairs = np.array(list(zip(x_data, y_data)))
-    # print(point_pairs)
-
     # user chooses K
     k = user_chooses_k()
     iterations = user_chooses_i()
 
-    alg = KMeansAlg(x_data, y_data, k, iterations)
+    alg = KMeansAlg(x_data, y_data, k, iterations, x, y)
     alg.run_algorithm()
-
-
-    # # --------------THIS NEEDS TO BE IMPLEMENTED-------------------
-    # # define KMeans object using K
-    # kmeans = KMeans(n_clusters=k)
-    #
-    # # process kernel length, width pairs
-    # kmeans.fit(point_pairs)
-    #
-    # # define and print centroid locations
-    # centroids = kmeans.cluster_centers_
-    # # print(centroids)
-    #
-    # labels = kmeans.labels_
-    # # print(labels)
-    # # -------------------------------------------------------------
-    #
-    # # process each record
-    # # "(g)reen(.) style
-    # colors = 5 * ["g.", "r.", "c.", "b.", "y.", "m.", "k."]
-    #
-    # # plot and show KMeans grouping
-    # for i in range(len(point_pairs)):
-    #     plt.plot(point_pairs[i][0], point_pairs[i][1], colors[labels[i]], markersize=6, label='_nolegend_')
-    #
-    # plt.plot(centroids[0][0], centroids[0][1], 'k+', markersize=14, label='Centroid')
-    # for i in range(1, k):
-    #     plt.plot(centroids[i][0], centroids[i][1], 'k+', markersize=14)
-    #
-    # plt.xlabel('Length of Kernel')
-    # plt.ylabel('Width of Kernel')
-    # plt.title('Seed Kernel (Length vs. Height) [KMeans = 2]')
-    # # plt.legend(loc='upper right', bbox_to_anchor=(0.95, 0.00001))
-    # plt.show()
 
 
 def setup_data_set_console_view():
@@ -82,7 +39,7 @@ def user_choose_data_set():
         2: 'test_data.csv'
     }
     user_choice = '-1'
-    while not (user_choice == '0' or user_choice == '1'):
+    while not (user_choice == '1' or user_choice == '2'):
         user_choice = input("\nWhich data set? Training or Test?\n\t1. Training\n\t2. Test\n>> ")
         data_set = data_sets.get(int(user_choice), "Try Again")
     return pd.read_csv(data_set)
@@ -92,41 +49,49 @@ def user_choose_x_y_attributes():
     attributes = {
         1: "REGION-CENTROID-COL",
         2: "REGION-CENTROID-ROW",
-        3: "INTENSITY-MEAN",
-        4: "RAWRED-MEAN",
-        5: "RAWBLUE-MEAN",
-        6: "RAWGREEN-MEAN",
-        7: "EXRED-MEAN",
-        8: "EXBLUE-MEAN",
-        9: "EXGREEN-MEAN",
-        10: "VALUE-MEAN",
-        11: "SATURATION-MEAN",
-        12: "HUE-MEAN"
+        3: "VEDGE-MEAN",
+        4: "VEDGE-SD",
+        5: "HEDGE-MEAN",
+        6: "HEDGE-SD",
+        7: "INTENSITY-MEAN",
+        8: "RAWRED-MEAN",
+        9: "RAWBLUE-MEAN",
+        10: "RAWGREEN-MEAN",
+        11: "EXRED-MEAN",
+        12: "EXBLUE-MEAN",
+        13: "EXGREEN-MEAN",
+        14: "VALUE-MEAN",
+        15: "SATURATION-MEAN",
+        16: "HUE-MEAN"
     }
     user_choice_y = '-1'
     while True:
         print(
             "\n\t1: REGION-CENTROID-COL"
             "\n\t2: REGION-CENTROID-ROW"
-            "\n\t3: INTENSITY-MEAN"
-            "\n\t4: RAWRED-MEAN"
-            "\n\t5: RAWBLUE-MEAN"
-            "\n\t6: RAWGREEN-MEAN"
-            "\n\t7: EXRED-MEAN"
-            "\n\t8: EXBLUE-MEAN"
-            "\n\t9: EXGREEN-MEAN"
-            "\n\t10: VALUE-MEAN"
-            "\n\t11: SATURATION-MEAN"
-            "\n\t12: HUE-MEAN"
+            "\n\t3: VEDGE-MEAN"
+            "\n\t4: VEDGE-SD"
+            "\n\t5: HEDGE-MEAN"
+            "\n\t6: HEDGE-SD"
+            "\n\t7: INTENSITY-MEAN"
+            "\n\t8: RAWRED-MEAN"
+            "\n\t9: RAWBLUE-MEAN"
+            "\n\t10: RAWGREEN-MEAN"
+            "\n\t11: EXRED-MEAN"
+            "\n\t12: EXBLUE-MEAN"
+            "\n\t13: EXGREEN-MEAN"
+            "\n\t14: VALUE-MEAN"
+            "\n\t15: SATURATION-MEAN"
+            "\n\t16: HUE-MEAN"
         )
         user_choice_x = input("\nChoose an attribute for X >> ")
         if not is_digit(user_choice_x):
             continue
-        elif 0 < int(user_choice_x) < 13:
+        elif 0 < int(user_choice_x) < 17:
             user_choice_y = input("Choose an attribute for Y >> ")
             if not is_digit(user_choice_y):
                 continue
-            elif 0 < int(user_choice_y) < 13:
+            elif 0 < int(user_choice_y) < 17:
                 break
     x_attribute = attributes.get(int(user_choice_x))
     y_attribute = attributes.get(int(user_choice_y))
